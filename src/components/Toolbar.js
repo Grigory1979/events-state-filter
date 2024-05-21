5888978c9a6d2c6721bfbe41ee0e28f504e8a7a6
Grigory <weissman1979@gmail.com>
2024-05-21 18:15:38 +0400

    commit

---
+++ src/components/Toolbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Toolbar = ({ filters, selected, onSelectFilter }) => {
+
+  console.log(onSelectFilter)
+  return (
+    <div>
+    {filters.map(filter => (
+      <button
+        key={filter}
+        onClick={() => onSelectFilter(filter)}
+        style={{ fontWeight: filter === selected ? 'bold' : 'normal' }}
+      >
+        {filter}
+      </button>
+    ))}
+  </div>
+  )
+
+
+
+}
+
+
+export default Toolbar;
